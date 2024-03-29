5a816cd5eb1e33faa76f856da5826234c3c4353d
Rafia Rahman <rafiarahman2511@gmail.com>
2024-03-29 10:24:46 +0600

    adding my git

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -9,8 +9,8 @@ const Book = ({book}) => {
     <img src={image} alt="books" className="rounded-xl w-60"/>
   </figure>
   <div>
-    <button className="px-5 py-2 font-extrabold border bg-green-300">{Young_or_Adult}</button>
-    <button className="px-5 py-2 font-extrabold border bg-green-300 mr-10">{publisher}</button>
+    <button className="px-5 py-2 font-extrabold border bg-green-300 rounded">{Young_or_Adult}</button>
+    <button className="px-5 py-2 font-extrabold border bg-green-300 mr-10 rounded">{publisher}</button>
   </div>
   
   <div className="card-body items-center text-center">
@@ -18,7 +18,6 @@ const Book = ({book}) => {
     
     <h2 className="card-title">{bookName}</h2>
     <p>{author}</p>
-    <p>{review}</p>
     <p>{totalPages}</p>
     <div className="flex justify-between gap-16">
         <p>{category}</p>
