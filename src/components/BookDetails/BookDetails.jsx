1f122a960d232cb9aebf758a52f8b883ba8748fc
Rafia Rahman <rafiarahman2511@gmail.com>
2024-03-29 22:48:58 +0600

    adding read button

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,37 +1,45 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const BookDetails = () => {
     const books = useLoaderData();
-    const {bookId} = useParams()
+    const { bookId } = useParams()
     const idInt = parseInt(bookId);
     const book = books.find(book => book.bookId === idInt);
     console.log(bookId, books);
 
-    const { image, bookName, author, review, totalPages, rating, category, Young_or_Adult, publisher, yearOfPublishing} = book;
-        
+    const handleReadBook = () =>{
+
+        toast('Hey!now you can read');
+    }
+
+    const { image, bookName, author, review, totalPages, rating, category, Young_or_Adult, publisher, yearOfPublishing } = book;
+
     return (
         <div>
             <div className="card card-side bg-base-100 shadow-xl">
-  <figure><img src={image} alt="Books"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">{bookName}</h2>
-    <p>{author}</p>
-    <hr />
-    <p>{category}</p>
-    <hr />
-    <p> Review: {review}</p>
-    <p>Tag: {Young_or_Adult}</p>
-    <p>{totalPages}</p>
-    <p>Publisher: {publisher}</p>
-    <p>Year of publish: {yearOfPublishing}</p>
-    <p>Rating: {rating}</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-info text-white">Read</button>
-      <button className="btn btn-info text-white">Wishlist</button>
-    </div>
-  </div>
-</div>
+                <figure><img src={image} alt="Books" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{bookName}</h2>
+                    <p>{author}</p>
+                    <hr />
+                    <p>{category}</p>
+                    <hr />
+                    <p> Review: {review}</p>
+                    <p>Tag: {Young_or_Adult}</p>
+                    <p>{totalPages}</p>
+                    <p>Publisher: {publisher}</p>
+                    <p>Year of publish: {yearOfPublishing}</p>
+                    <p>Rating: {rating}</p>
+                    <div className="card-actions justify-end">
+                        <button onClick={handleReadBook} className="btn btn-info text-white">Read</button>
+                        <button className="btn btn-info text-white">Wishlist</button>
+                    </div>
+                </div>
+            </div>
+            <ToastContainer />
         </div>
     );
 };
